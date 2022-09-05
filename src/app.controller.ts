ee0bcf13413ecba4dc54b4e3bec2914e3806c02a
Dirga Yasa <cekingx@gmail.com>
2022-09-05 13:13:09 +0800

    add endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/too-many')
+  @HttpCode(429)
+  tooMany() {
+    return "too many request";
   }
 }
